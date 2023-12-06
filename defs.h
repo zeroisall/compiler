380c45815c42bfac5a7027f87eee3e959bb82d95
zzx <zzxemail>
2023-12-06 17:39:42 +0800

    Save daily work

---
+++ defs.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// tokens
+enum etoken
+{
+    T_PLUS,
+    T_MINUS,
+    T_STAR,
+    T_SLASH,
+    T_INTLIT
+};
+
+// token struct
+struct token
+{
+    int token;
+    int intvalue;
+};
